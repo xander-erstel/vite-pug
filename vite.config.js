192a8bbf152ddb50aa041c8cb5de597bc29e40b3
xander <xander@erstel.ru>
2024-04-18 11:30:19 +0300

    Initial project

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from 'vite'
+import pugPlugin from 'vite-plugin-pug'
+
+const {resolve} = require('path')
+
+export default defineConfig({
+    plugins: [pugPlugin(undefined, {pagesUrl: './pages/'})],
+
+    server: {port: 9001},
+    optimizeDeps: {
+        // include: ['node_modules/gsap/ScrollTrigger'],
+    },
+
+    build: {
+        rollupOptions: {
+            input: {
+                // need a better way to template
+                main: resolve(__dirname, 'index.html'),
+                /*page: resolve(__dirname, 'pages/page.html'),
+                page2: resolve(__dirname, 'pages/page2.html'), */
+            },
+        },
+    },
+})
