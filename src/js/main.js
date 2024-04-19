0337b2eb6873a6ad114afa105d121e9750655df6
xander <xander@erstel.ru>
2024-04-19 15:47:56 +0300

    Fix Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 import jQuery from 'jquery';
 window.$ = jQuery;
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+});
 $(document).ready(function () {
     console.log('jquery js loaded');
 })
